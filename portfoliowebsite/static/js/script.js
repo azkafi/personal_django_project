83f14c99611c6faac9c12af367638696e14ed1a1
azkafi <aktaruzzmankafi017@gmail.com>
2024-10-05 09:44:32 +0600

    updating header javascript chash and javascript for project menu filtering

--- portfoliowebsite/static/js/script.js
+++ portfoliowebsite/static/js/script.js
@@ -49,18 +49,19 @@ parallaxContainer.addEventListener('mouseleave', function () {
 
 
 //PORTFOLIO FILTER
+
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filter_column");
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
-function addClass(element, name) {
+function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -69,7 +70,7 @@ function addClass(element, name) {
   }
 }
 
-function removeClass(element, name) {
+function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -81,15 +82,13 @@ function removeClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-
 // Add active class to the current button (highlight it)
-
-var header = document.getElementById("myBtnContainer");
-var btns = header.getElementsByClassName("btn");
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
 }
